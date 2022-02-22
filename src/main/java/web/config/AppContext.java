7e483de03a344896b080313710d78fb3c25f6b0c
Evgeniy Strigo <evgstrigo@mail.ru>
2022-02-22 20:34:26 +0300

    evening 22.02

---
+++ src/main/java/web/config/AppContext.java
@@ -0,0 +1,14 @@
+package web.config;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppContext implements WebMvcConfigurer {
+
+
+
+}
